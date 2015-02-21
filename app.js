70a7491116783e96359cff9fd06057d245c877d4
Nina Baculinao <nb2406@columbia.edu>
2015-02-21 01:03:37 -0500

    feat(app.js): start a simple server route to localhost:3000

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 var express = require( 'express' );
+var morgan = require('morgan');
+
 var app = express();
+
+app.use(morgan('dev'));
+
+app.listen(3000, function() {
+	console.log("server listening");
+});
+
+app.get('/', function (req,res) {
+	res.send('hello, world!')
+})
