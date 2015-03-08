1822d21d884fffb511013975b3712971d13251e5
Nina Baculinao <nb2406@columbia.edu>
2015-03-08 17:15:35 -0400

    feat(app.js): add socket.io to the server

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ var express = require( 'express' );
 var morgan = require('morgan');
 var swig = require('swig');
 var routes = require('./routes/');
+var socketio = require('socket.io');
 
 var app = express();
-
-app.use(morgan('dev'));
-app.use('/', routes);
-app.use(express.static(__dirname + '/public'));
+// var server = app.listen(3000);
 
 // function to actually render html
 app.engine('html', swig.renderFile);
@@ -16,7 +14,6 @@ app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 // set views path to views folder (where we store templates)
 app.set('views', __dirname + '/views'); // __dirname +
-
 // Swig will cache templates for you, but you can disable
 // that and use Express's caching instead, if you like:
 app.set('view cache', false);
@@ -26,12 +23,28 @@ swig.setDefaults({ cache: false });
 // NOTE: You should always cache templates in a production environment.
 // Don't leave both of these to `false` in production!
 
+app.use(morgan('dev'));
+// app.use('/', routes);
+app.use(express.static(__dirname + '/public'));
+
 // app.get('/', function (req, res) {
 //   // res.send('hello, world!')
 //   var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 //   res.render('index', {title: 'Hall of Fame', people: people});
 // });
 
-app.listen(3000, function() {
-  console.log("server listening on http://localhost:3000");
+// app.listen(3000, function() {
+//   console.log("server listening on http://localhost:3000");
+// });
+
+var server = app.listen(1234, function() {
+
+	var host = server.address().address;
+	var port = server.address().port;
+
+	console.log('Listening at http://localhost:%s', port);
 });
+
+var io = socketio.listen(server);
+
+app.use('/', routes(io));
